c456e6c9a6604ee49755d1cb9e541bdaf80e37ed
thebestbadfriend <thebestbadfriend@gmail.com>
2020-04-22 00:01:30 -0500

    Add project files.

---
+++ Game_QuestionMark.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//Game game;
+
+int main()
+{
+	while (/*game is running*/ true) {
+		/*
+		game.HandleEvents();
+		game.UpdateFrame();
+		game.RenderFrame();
+		game.CleanupFrame(); //maybe?
+		*/
+	}
+
+	//game.Shutdown(); //free up resources used by the game, handle any errors, perform any other necessary cleanup
+	//quit the application
+}
